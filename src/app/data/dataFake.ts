674ae9abbd83bee3a3e90f377cb95f3f8c0b7bb3
amantomoura <amantomoura@live.com>
2023-05-11 20:19:54 -0300

    first commit

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,28 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Calvin Harris revisita clássicos em novo single",
+    "description": " 'Miracle' com participação de Ellie Goulding homenageia grandes nomes como Robert Miles, e Armin Van Buuren. ",
+    "photoCover":"https://i.ytimg.com/vi/961v0E3b01g/maxresdefault.jpg"
+  },
+  {
+    "id":"2",
+    "title": "A máquina! Conheça a nova DDJ FLX-10 da Pioneer.",
+    "description": "Com a intenção de substituir a já datada DDJ 1000, a nova adição da linha Flex conta com botões físicos para os Stems função não tão nova assim mas que vem ganhando destaque no mundo dos DJS.",
+    "photoCover":"https://www.pioneerdj.com/-/media/pioneerdj/images/products/controller/ddj-flx10/pc-cgi_angle.png"
+  },
+  {
+    "id":"3",
+    "title": "Beat: Série do Prime Vídeo promete e entrega muito drama e música.",
+    "description": "Com muito drama e música eletrônica a nova série do Prime Video é um deleite para os fãs de Techno.",
+    "photoCover":"https://images-na.ssl-images-amazon.com/images/S/pv-target-images/b5a76d41ef81e9f06b022273d9e1604251a14083c92efd6496871f446e9390d7._RI_TTW_.jpg"
+  },
+
+  {
+    "id":"4",
+    "title": "Date My Playlist: MTV inova mais uma vez e lança o 'tinder musical' confira.",
+    "description": "Tentando voltar as origens mas sem deixar de lado o amor por reality, canal americano inova ao decidir a escolha do parceiro ideal através das músicas.",
+    "photoCover":"https://i.ytimg.com/vi/Xs-S9ElHMmA/maxresdefault.jpg"
+  }
+
+]
